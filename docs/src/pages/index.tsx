b32f330fb2eedef404488602298dbdcd1e4b1159
kartikeya <67143288+kartikeya-27@users.noreply.github.com>
2026-08-06 08:33:34 +0530

    docs: rebrand, restructure and move to pnpm (#60)

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -42,7 +42,7 @@ function HomepageHeader(): ReactNode {
   return (
     <header className="hero-banner">
       <div className="container">
-        <img src="img/icon.png" alt="reclink" className="hero-logo" style={{width: 120, height: 120}} />
+        <img src="img/logo.png" alt="reclink" className="hero-logo" style={{width: 120, height: 120}} />
         <Heading as="h1">{siteConfig.title}</Heading>
         <p>{siteConfig.tagline}</p>
         <div className="install-command">pip install reclink</div>
